c3dd04b474c17ed93f12428998f25b5f5df5f038
bethjm <b.jeanne.mills@gmail.com>
2023-05-22 12:53:42 -0400

    added state and conditional rendering to error mdoal

--- 01-starting-project/src/components/Users/AddUser.js
+++ 01-starting-project/src/components/Users/AddUser.js
@@ -7,13 +7,22 @@ import ErrorModal from "../UI/ErrorModal";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (greater than 1)",
+      });
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non empty values)",
+      });
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -31,7 +40,7 @@ const AddUser = (props) => {
 
   return (
     <div>
-      <ErrorModal title="An error occured!" message="Something went wrong" />
+      {error && <ErrorModal title={error.title} message={error.message} />}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
